f520008e78086774fbd5fddb86584b9e0825093e
Alex <anmunoz24@gmail.com>
2020-05-11 16:28:48 -0500

    heroku deployment

--- client/src/redux/action.js
+++ client/src/redux/action.js
@@ -20,30 +20,3 @@ export const addArtist = (artist) => (dispatch) => {
 }
 
 
-// export const addArtist = (artist) => {
-//     console.log(artist, "artists")
-//     return {
-//         type: 'ADD_ARTIST',
-//         value: artist
-//     }
-// }
-
-// export const getArtist = () => dispatch => {
-//     dispatch(setItemsLoading())
-//     axios
-//         .get('/api/artists')
-//         .then(res => {   
-//             dispatch({
-//             type: "GET_ARTISTS",
-//             payload: res.data
-//         })
-//         console.log(res.data)
-//         }
-//     )
-// }
-
-// export const setItemsLoading = () => {
-//     return {
-//         type: "ITEMS_LOADING"
-//     }
-// }
